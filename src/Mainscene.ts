4894a5a8e8f46075fe13b6b500521eb1fe92832f
kangspa <111270327+kangspa@users.noreply.github.com>
2025-06-09 13:00:34 +0900

    edit:Mainscene.ts - 메인 이벤트들 등록 완료

--- src/Mainscene.ts
+++ src/Mainscene.ts
@@ -2,16 +2,27 @@
 import Phaser from 'phaser';
 import fetchAndParseHTML from './fetchUrls';
 
+interface PipeWithIndex extends Phaser.Physics.Arcade.Sprite {
+  pipeIndex: number;
+}
+
 export default class MainScene extends Phaser.Scene {
+  private targetUrl = 'https://kangspa.github.io/'; // 대상 URL
+
   private player!: Phaser.Physics.Arcade.Sprite;
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
   private lastDirection: 'left' | 'right' = 'right';
 
   private platforms!: Phaser.Physics.Arcade.StaticGroup;
+  private pipeH!: Phaser.Physics.Arcade.Sprite;
+  private pipeR!: Phaser.Physics.Arcade.Sprite;
   private pipes!: Phaser.Physics.Arcade.StaticGroup;
   private onPipeEnter?: (url: string) => void;
+  private isEnteringPipe = false;
 
   private urlList!: string[]; // URL 목록을 저장할 프로퍼티
+  private pageCount!: number; // 페이지 수를 저장할 프로퍼티
+  private currentPage!: number; // 현재 페이지를 저장할 프로퍼티
 
   constructor(onPipeEnter?: (url: string) => void) {
     super('MainScene');
@@ -28,8 +39,9 @@ export default class MainScene extends Phaser.Scene {
   }
   
   private async loadUrls() {
-    const targetUrl = 'https://kangspa.github.io/'; // 대상 URL
-    this.urlList = await fetchAndParseHTML(targetUrl);
+    this.urlList = await fetchAndParseHTML(this.targetUrl);
+    this.pageCount = Math.ceil(this.urlList.length / 5) - 1;
+    this.currentPage = 0; // 현재 페이지 초기화
     console.log('Fetched URLs in scene:', this.urlList);
   }
 
@@ -75,29 +87,63 @@ export default class MainScene extends Phaser.Scene {
     });
 
     this.physics.add.collider(this.player, this.platforms);
+    this.cursors = (this.input.keyboard as Phaser.Input.Keyboard.KeyboardPlugin).createCursorKeys();
 
     // prev 블럭 생성
-    const prevBlock = this.physics.add.staticSprite(160, 150, 'blocks', 4);
-    this.physics.add.collider(this.player, prevBlock, () => {
-      const iframe = document.querySelector('iframe') as HTMLIFrameElement;
-      iframe?.contentWindow?.postMessage('click-prev', 'https://kangspa.github.io');
+    const prevBlock = this.physics.add.staticSprite(192, 150, 'blocks', 4);
+    this.physics.add.collider(this.player, prevBlock, (obj1, ) => {
+      const player = obj1 as Phaser.Physics.Arcade.Sprite;
+      const body = player.body as Phaser.Physics.Arcade.Body;
+      if (body.touching.up && !this.isEnteringPipe) {
+        const iframe = document.querySelector('iframe') as HTMLIFrameElement;
+        iframe?.contentWindow?.postMessage('click-prev', 'https://kangspa.github.io');
+        if (this.currentPage !== 0) this.currentPage--;
+      };
     });
 
     // next 블럭 생성
-    const nextBlock = this.physics.add.staticSprite(256, 150, 'blocks', 5);
-    this.physics.add.collider(this.player, nextBlock, () => {
-      const iframe = document.querySelector('iframe') as HTMLIFrameElement;
-      iframe?.contentWindow?.postMessage('click-next', 'https://kangspa.github.io');
+    const nextBlock = this.physics.add.staticSprite(288, 150, 'blocks', 5);
+    this.physics.add.collider(this.player, nextBlock, (obj1, ) => {
+      const player = obj1 as Phaser.Physics.Arcade.Sprite;
+      const body = player.body as Phaser.Physics.Arcade.Body;
+      if (body.touching.up && !this.isEnteringPipe) {
+        const iframe = document.querySelector('iframe') as HTMLIFrameElement;
+        iframe?.contentWindow?.postMessage('click-next', 'https://kangspa.github.io');
+        if (this.pageCount !== this.currentPage) this.currentPage++;
+      };
+    });
+
+    // 스크롤 업 다운 블럭 생성
+    const upBlock = this.physics.add.staticSprite(240, 90, 'blocks', 2);
+    this.physics.add.collider(this.player, upBlock, (obj1, ) => {
+      const player = obj1 as Phaser.Physics.Arcade.Sprite;
+      const body = player.body as Phaser.Physics.Arcade.Body;
+      if (body.touching.up) {
+        const iframe = document.querySelector('iframe') as HTMLIFrameElement;
+        iframe?.contentWindow?.postMessage('scroll-up', 'https://kangspa.github.io');
+      };
+    });
+
+    const downBlock = this.physics.add.staticSprite(240, 150, 'blocks', 3);
+    this.physics.add.collider(this.player, downBlock, (obj1, ) => {
+      const player = obj1 as Phaser.Physics.Arcade.Sprite;
+      const body = player.body as Phaser.Physics.Arcade.Body;
+      if (body.touching.up) {
+        const iframe = document.querySelector('iframe') as HTMLIFrameElement;
+        iframe?.contentWindow?.postMessage('scroll-down', 'https://kangspa.github.io');
+      };
     });
 
     // 파이프 추가
-    const pipeH = this.physics.add.sprite(66, 202, 'pipes', 5);
-    this.physics.add.collider(pipeH, this.platforms);
+    this.pipeH = this.physics.add.staticSprite(66, 202, 'pipes', 5);
+    this.physics.add.collider(this.pipeH, this.platforms);
+    this.physics.add.collider(this.pipeH, this.player);
+
+    this.pipeR = this.physics.add.staticSprite(130, 202, 'pipes', 6);
+    this.physics.add.collider(this.pipeR, this.platforms);
+    this.physics.add.collider(this.pipeR, this.player);
 
     // 파이프 생성
-    interface PipeWithIndex extends Phaser.Physics.Arcade.Sprite {
-      pipeIndex: number;
-    }
     this.pipes = this.physics.add.staticGroup();
 
     const pipePositions = [350, 446, 542, 638, 734];
@@ -108,15 +154,7 @@ export default class MainScene extends Phaser.Scene {
     });
 
     this.physics.add.collider(this.pipes, this.platforms);
-
-    // 파이프 충돌 시 이벤트 발사
-    this.physics.add.overlap(this.player, this.pipes, (_player, pipe) => {
-      const pipeWithIndex = pipe as PipeWithIndex;
-      const index = pipeWithIndex.pipeIndex;
-      this.onPipeEnter?.(this.urlList[index]);
-    });
-
-    this.cursors = (this.input.keyboard as Phaser.Input.Keyboard.KeyboardPlugin).createCursorKeys();
+    this.physics.add.collider(this.pipes, this.player);
   }
 
   update() {
@@ -142,5 +180,45 @@ export default class MainScene extends Phaser.Scene {
     if (this.cursors.up?.isDown && body.onFloor()) {
         this.player.setVelocityY(-300);
     }
+
+    if (this.cursors.down?.isDown && body.blocked.down) {
+      const playerBounds = this.player.getBounds();
+
+      for (const pipe of this.pipes.getChildren()) {
+        const p = pipe as PipeWithIndex;
+        const pipeBounds = p.getBounds();
+
+        const isOnPipe =
+          playerBounds.bottom === pipeBounds.top &&
+          playerBounds.right > pipeBounds.left &&
+          playerBounds.left < pipeBounds.right;
+
+        if (isOnPipe && !this.isEnteringPipe) {
+          this.isEnteringPipe = true;
+          const index = p.pipeIndex;
+          const url = this.urlList[index + this.currentPage * 5];
+          this.onPipeEnter?.(url);
+          break; // 다른 파이프 검사하지 않음
+        }
+      }
+      // pipeH에 진입 이벤트 발생하는지 확인
+      const pipeHBounds = this.pipeH.getBounds();
+      const isOnPipeH =
+        playerBounds.bottom === pipeHBounds.top &&
+        playerBounds.right > pipeHBounds.left &&
+        playerBounds.left < pipeHBounds.right;
+      if (isOnPipeH && this.isEnteringPipe) {
+        this.isEnteringPipe = false;
+        const iframe = document.querySelector('iframe') as HTMLIFrameElement;
+        iframe?.contentWindow?.postMessage('go-back', 'https://kangspa.github.io');
+      }
+      // pipeR에 진입 이벤트 발생하는지 확인
+      const pipeRBounds = this.pipeR.getBounds();
+      const isOnPipeR =
+        playerBounds.bottom === pipeRBounds.top &&
+        playerBounds.right > pipeRBounds.left &&
+        playerBounds.left < pipeRBounds.right;
+      if (isOnPipeR) window.location.reload();
+    };
   }
 }
